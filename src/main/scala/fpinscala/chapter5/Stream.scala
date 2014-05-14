586ca371c731841bc149d5d9cf32ac87763d85b9
Federico ragona <federico.ragona@gmail.com>
2014-05-14 23:26:38 +0200

    Add filter,map,flatMap,append,foldRight,forAll

--- src/main/scala/fpinscala/chapter5/Stream.scala
+++ src/main/scala/fpinscala/chapter5/Stream.scala
@@ -1,36 +1,70 @@
 package fpinscala
 package chapter5
 
+import Stream._
+
 sealed trait Stream[+A] {
+
+  def append[B >: A](other: => Stream[B]): Stream[B] =
+    foldRight(other)(cons(_, _))
+
+  def drop(n: Int): Stream[A] =
+    this match {
+      case Empty => empty
+      case Cons(h, t) =>
+        if(n > 0) t().drop(n -1)
+        else this
+    }
+
+  def filter(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a, b) => if(p(a)) cons(a, b) else b)
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((a, b) => f(a) append b)
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def forAll(p: A => Boolean): Boolean =
+    this match {
+      case Empty => true
+      case Cons(h, t) =>
+        p(h()) && t().forAll(p)
+    }
+
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
   }
 
+  def headOption2: Option[A] =
+    foldRight(None: Option[A])((h, _) => Some(h))
+
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((a, b) => cons(f(a), b))
+
   def take(n: Int): Stream[A] =
     this match {
-      case Empty => Empty
+      case Empty => empty
       case Cons(h, t) =>
-        if(n > 0) Stream.cons(h(), t().take(n - 1))
-        else Empty
+        if(n > 0) cons(h(), t().take(n - 1))
+        else empty
     }
 
   def takeWhile(p: A => Boolean): Stream[A] =
     this match {
-      case Empty => Empty
+      case Empty => empty
       case Cons(h, t) =>
-        val hd = h()
-        if(p(hd)) Stream.cons(hd, t().takeWhile(p))
-        else Empty
+        lazy val hd = h()
+        if(p(hd)) cons(hd, t().takeWhile(p))
+        else empty
     }
 
-  def drop(n: Int): Stream[A] =
-    this match {
-      case Empty => Empty
-      case Cons(h, t) =>
-        if(n > 0) t().drop(n -1)
-        else this
-    }
+  def takeWhile2(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a, b) => if(p(a)) cons(a, b) else b)
 
   def toList: List[A] =
     this match {
--- src/test/scala/fpinscala/chapter5/StreamSpec.scala
+++ src/test/scala/fpinscala/chapter5/StreamSpec.scala
@@ -7,32 +7,95 @@ class StreamSpec extends UnitSpec {
 
   implicit val positive = Arbitrary(Gen.choose(0, 20))
 
-  it should "convert to list" in {
-    forAll { (n: Int) =>
-      val expected = (0 until n).toList
-      Stream(expected:_*).toList shouldBe expected
-    }
-  }
-
-  it should "take the first n elements" in {
-    forAll { (n: Int, m: Int) =>
-      val expected = (0 until n).toList
-      Stream(expected:_*).take(m).toList shouldBe expected.take(m)
-    }
-  }
-
-  it should "drop the first n elements" in {
+  it should "drop" in {
     forAll { (n: Int, m: Int) =>
       val expected = (0 until n).toList
       Stream(expected:_*).drop(m).toList shouldBe expected.drop(m)
     }
   }
 
-  it should "take while the predicate holds true" in {
+  it should "filter" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+      val p = (x: Int) => x % 5 == 0
+
+      Stream(expected:_*).filter(p).toList shouldBe expected.filter(p)
+    }
+  }
+
+  it should "flatMap" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+      val f = (x: Int) => Stream(x * 5)
+      val g = (x: Int) => List(x * 5)
+
+      Stream(expected:_*).flatMap(f).toList shouldBe expected.flatMap(g)
+    }
+  }
+
+  it should "forAll" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+      val p1 = (x: Int) => x < n / 2
+      val p2 = (x: Int) => x <= n
+
+      Stream(expected:_*).forAll(p1) shouldBe expected.forall(p1)
+      Stream(expected:_*).forAll(p2) shouldBe expected.forall(p2)
+    }
+  }
+
+  it should "headOption" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+
+      Stream(expected:_*).headOption shouldBe expected.headOption
+    }
+  }
+
+  it should "headOption2" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+
+      Stream(expected:_*).headOption2 shouldBe expected.headOption
+    }
+  }
+
+  it should "map" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+      val f = (x: Int) => x * 5
+
+      Stream(expected:_*).map(f).toList shouldBe expected.map(f)
+    }
+  }
+
+  it should "take" in {
+    forAll { (n: Int, m: Int) =>
+      val expected = (0 until n).toList
+      Stream(expected:_*).take(m).toList shouldBe expected.take(m)
+    }
+  }
+
+  it should "takeWhile" in {
     forAll { (n: Int) =>
       val expected = (0 until n).toList
       val p = (n: Int) => n < 5
       Stream(expected:_*).takeWhile(p).toList shouldBe expected.takeWhile(p)
+    }
+  }
+
+  it should "takeWhile2" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+      val p = (n: Int) => n < 5
+      Stream(expected:_*).takeWhile2(p).toList shouldBe expected.takeWhile(p)
+    }
+  }
+
+  it should "toList" in {
+    forAll { (n: Int) =>
+      val expected = (0 until n).toList
+      Stream(expected:_*).toList shouldBe expected
     }
   }
 }
